ba64ad805db1da11fe526daa3a18fc7199fbd372
Thedineshk24 <thedineshk24@gmail.com>
2020-09-16 22:53:41 +0530

    Updates

---
+++ precache-manifest.f20a4f36cb4df997c2cbcd4530477c13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7398991c446953a7ad2ca16c6a826b8b",
+    "url": "/dineshkatariya/index.html"
+  },
+  {
+    "revision": "f315e3f9176109a24159",
+    "url": "/dineshkatariya/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "98175f7a81d3e641ec74",
+    "url": "/dineshkatariya/static/js/2.29e034a0.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/dineshkatariya/static/js/2.29e034a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f315e3f9176109a24159",
+    "url": "/dineshkatariya/static/js/main.70866efb.chunk.js"
+  },
+  {
+    "revision": "3734f423d88656c4db85",
+    "url": "/dineshkatariya/static/js/runtime-main.d35589fd.js"
+  }
+]);
