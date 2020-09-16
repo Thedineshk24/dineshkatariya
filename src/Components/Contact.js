5229d324fc7ad73bff3eac8728306e8efc94bd2a
Thedineshk24 <thedineshk24@gmail.com>
2020-09-16 21:50:25 +0530

    port-folio website

---
+++ src/Components/Contact.js
@@ -0,0 +1,139 @@
+import React, { Component } from "react";
+import emailjs from "emailjs-com";
+// import apiKeys from "../apikeys";
+
+// const onSubmit = (e) => {
+//   e.preventDefault(); // Prevents default refresh by the browser
+//   emailjs
+//     .sendForm("gmail", apiKeys.TEMPLATE_ID, e.target, apiKeys.USER_ID)
+//     .then(
+//       (result) => {
+//         alert("Message Sent, I'll get back to you shortly", result.text);
+//       },
+//       (error) => {
+//         alert("An error occured, Plese try again", error.text);
+//       }
+//     );
+// };
+class Contact extends Component {
+  render() {
+    if (this.props.data) {
+      var name = this.props.data.name;
+      var street = this.props.data.address.street;
+      var city = this.props.data.address.city;
+      var state = this.props.data.address.state;
+      var zip = this.props.data.address.zip;
+      var phone = this.props.data.phone;
+      var email = this.props.data.email;
+      var message = this.props.data.contactmessage;
+    }
+
+    return (
+      <section id="contact">
+        <div className="row section-head">
+          <div className="two columns header-col">
+            <h1>
+              <span>Get In Touch.</span>
+            </h1>
+          </div>
+
+          <div className="ten columns">
+            <p className="lead">{message}</p>
+            <p style={{ color: "red" }}>
+              Warning: Contact form not yet configured, please contact me via
+              normal email for now Or from other sites like linkedin twitter
+              instagram.
+            </p>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="eight columns">
+            <form className="form">
+              <fieldset>
+                <div>
+                  <label htmlFor="contactName">
+                    Name <span className="required">*</span>
+                  </label>
+                  <input
+                    type="text"
+                    defaultValue=""
+                    size="35"
+                    id="contactName"
+                    name="contactName"
+                  />
+                </div>
+
+                <div>
+                  <label htmlFor="contactEmail">
+                    Email <span className="required">*</span>
+                  </label>
+                  <input
+                    type="text"
+                    defaultValue=""
+                    size="35"
+                    id="contactEmail"
+                    name="contactEmail"
+                  />
+                </div>
+
+                <div>
+                  <label htmlFor="contactSubject">Subject</label>
+                  <input
+                    type="text"
+                    defaultValue=""
+                    size="35"
+                    id="contactSubject"
+                    name="contactSubject"
+                  />
+                </div>
+
+                <div>
+                  <label htmlFor="contactMessage">
+                    Message <span className="required">*</span>
+                  </label>
+                  <textarea
+                    cols="50"
+                    rows="15"
+                    id="contactMessage"
+                    name="contactMessage"></textarea>
+                </div>
+
+                <div>
+                  <button className="submit" type="submit">
+                    Submit
+                  </button>
+                  {/* <span id="image-loader">
+                    <img alt="" src="images/loader.gif" />
+                  </span> */}
+                </div>
+              </fieldset>
+            </form>
+            {/* <div id="message-warning"> Error boy</div>
+            <div id="message-success">
+              <i className="fa fa-check"></i>Your message was sent, thank you!
+              <br />
+            </div>
+          </div> */}
+          </div>
+
+          <aside className="four columns footer-widgets">
+            <div className="widget widget_contact">
+              <h4>Address and Phone</h4>
+              <p className="address">
+                {name}
+                <br />
+                {street} <br />
+                {city}, {state} <p style={{ color: "lime" }}>{zip}</p>
+                <br />
+                <span>{phone}</span>
+              </p>
+            </div>
+          </aside>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Contact;
